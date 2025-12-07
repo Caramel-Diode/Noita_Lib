a2ff5664f4880e06c08d3cbf4ecc856d1f2fb35b
bdtgd <2746671038@qq.com>
2025-12-08 00:52:03 +0800

    更新API名称

--- src/component/Orb/index.js
+++ src/component/Orb/index.js
@@ -1,15 +1,16 @@
 const Orb = (() => {
     embed(`#db.js`);
     OrbData.init();
-    const styleSheet = css(embed(`#base.css`));
+    const styleSheet = css(embed(`#base.css`), { name: "orb-base" });
     OrbData.orbUrls.then(v => {
-        styleSheet.insertRule(`:host{--bg:url(${v[0]})}`);
-        styleSheet.insertRule(String.raw`:host([orb\.id="red"]){--bg:url(${v[1]})}`);
-        styleSheet.insertRule(String.raw`:host([orb\.id="discovered"]){--bg:url(${v[2]})}`);
-        styleSheet.insertRule(String.raw`:host([orb\.id="evil"]){--bg:url(${v[3]})}`);
+        /** @type {CSSLayerBlockRule} */
+        const layer = styleSheet.cssRules[0];
+        layer.insertRule(`:host{--bg:url(${v[0]})}`);
+        layer.insertRule(String.raw`:host([orb\.id="red"]){--bg:url(${v[1]})}`);
+        layer.insertRule(String.raw`:host([orb\.id="discovered"]){--bg:url(${v[2]})}`);
+        layer.insertRule(String.raw`:host([orb\.id="evil"]){--bg:url(${v[3]})}`);
     });
-
-    return class HTMLNoitaOrbElement extends $class(Base, {
+    return class HTMLNoitaOrbElement extends $extends(Base, {
         /** @type {$ValueOption<String>} 禁用显示模式 */
         displayMode: { name: "display", $default: "#" },
         /** @type {$ValueOption<SpellId|SpellName|SpellAlias>} */
@@ -23,21 +24,21 @@ const Orb = (() => {
             if (id) this.orbId = id;
         }
 
+        static [$css] = { base: [styleSheet] };
+
         /**
          * @override
          * @see Base#contentUpdate
          */
-        contentUpdate() {
-            this.shadowRoot.innerHTML = "";
+        [$content]() {
             const { spellIcon, name } = (this.orbData = OrbData.data.get(this.orbId));
             if (this.orbId === "common" || this.orbId === "red") this.shadowRoot.append(h.div({ class: "insert" }, h.slot()));
             else this.shadowRoot.append(spellIcon);
             this.title = name;
-            super[$symbols.initStyle]([styleSheet]);
         }
 
         //prettier-ignore
         get [Symbol.toStringTag]() { return `HTMLNoitaMessageElement #${this.orbId}` }
     };
 })();
-customElements.define("noita-orb", freeze(Orb));
+h["noita-orb"] = freeze(Orb);
