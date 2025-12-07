2a6209db783ccbb389fe42f5faf2558d61aad1f6
bdtgd <2746671038@qq.com>
2025-12-07 23:32:48 +0800

    移除非必要的ployfill

--- src/public/polyfill.js
+++ src/public/polyfill.js
@@ -6,38 +6,38 @@
  * 取交集运算
  * @param {Set} set
  */
-Set.prototype.intersection ??= set => {
-    const result = new Set();
-    for (const e of set) if (this.has(e)) result.add(e);
-    return result;
-};
+// Set.prototype.intersection ??= set => {
+//     const result = new Set();
+//     for (const e of set) if (this.has(e)) result.add(e);
+//     return result;
+// };
 
 /**
  * 取并集运算
  * @param {Set} set
  */
-Set.prototype.union ??= set => new Set([...this, ...set]);
+// Set.prototype.union ??= set => new Set([...this, ...set]);
 
 /**
  * 取补集运算
  * @param {Set} set
  */
-Set.prototype.difference ??= set => {
-    const result = new Set([...this]);
-    for (const e of set) result.delete(e);
-    return result;
-};
+// Set.prototype.difference ??= set => {
+//     const result = new Set([...this]);
+//     for (const e of set) result.delete(e);
+//     return result;
+// };
 
 /**
  * 对应某些低版本的chromium内核浏览器需要这个函数的补丁
  */
 
 //prettier-ignore
-Promise.withResolvers ??=  () => {
-    let resolve, reject;
-    const promise = new Promise((res, rej) => (resolve = res, reject = rej));
-    return { promise, resolve, reject };
-};
+// Promise.withResolvers ??=  () => {
+//     let resolve, reject;
+//     const promise = new Promise((res, rej) => (resolve = res, reject = rej));
+//     return { promise, resolve, reject };
+// };
 
 {
     /**
@@ -60,17 +60,17 @@ Promise.withResolvers ??=  () => {
         }
     }
     
-    if (window.Iterator) {
+    // if (window.Iterator) {
         Reflect.defineProperty(Iterator.prototype, "chunks", {
             value: chunks,
             enumerable: false,
             configurable: false
         });
-    } else {
-        Reflect.defineProperty(Array.prototype.values().constructor.prototype, "chunks", {
-            value: chunks,
-            enumerable: false,
-            configurable: false
-        });
-    }
+    // } else {
+    //     Reflect.defineProperty(Array.prototype.values().constructor.prototype, "chunks", {
+    //         value: chunks,
+    //         enumerable: false,
+    //         configurable: false
+    //     });
+    // }
 }
