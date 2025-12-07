f62704e87335ff19df3b73e3283340c70233735f
bdtgd <2746671038@qq.com>
2025-12-07 23:01:39 +0800

    移除无用嵌入 调整嵌入顺序

--- src/embedList.js
+++ src/embedList.js
@@ -6,14 +6,14 @@ embed(`#public/cssTools.js`);
 embed(`#public/util.js`);
 embed(`#public/XML.js`);
 embed(`#public/CSV.js`);
-// embed(`#public/consoleCSS.js`);
-// embed(`#public/runtimeEnvironment.js`);
 embed(`#public/damageData.js`);
-// embed(`#public/save.js`);
 embed(`#public/translation.js`);
 
 // 组件库
 embed(`#component/Base/index.js`); // 基础
+embed(`#component/Inventory/index.js`); // 基础
+embed(`#component/InputRange/index.js`); // 基础
+embed(`#component/Material/index.js`); // 材料
 embed(`#component/Entity/index.js`); // 实体
 embed(`#component/Spell/index.js`); // 法术
 embed(`#component/Wand/index.js`); // 魔杖
@@ -21,6 +21,5 @@ embed(`#component/Perk/index.js`); // 天赋
 embed(`#component/Cursor/index.js`); // 光标
 embed(`#component/Message/index.js`); // 通知
 embed(`#component/Container/index.js`); // 容器
-embed(`#component/Material/index.js`); // 材料
 embed(`#component/Orb/index.js`); // 魔球
 embed(`#component/Status/index.js`); // 状态
