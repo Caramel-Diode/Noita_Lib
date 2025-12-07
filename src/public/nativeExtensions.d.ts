97ee4e6b171b62c95e8bffea1d028b7e89a551a8
bdtgd <2746671038@qq.com>
2025-12-07 22:45:17 +0800

    移除无效的和测试的文件

--- src/public/nativeExtensions.d.ts
+++
@@ -1,8 +0,0 @@
-interface Iterator<T, TReturn = any, TNext = any> {
-    /**
-     * Consumes this iterator as non-overlapping subsequences (chunks) of configurable size.
-     * @param size chunkSize The size of each chunk.
-     * @returns An iterator that yields arrays of elements from the original iterator.
-     */
-    chunks(size: Number): Iterator<Array<T>, TReturn, TNext>;
-}
