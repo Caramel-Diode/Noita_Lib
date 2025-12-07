971b724d9ad99253c483ed912dae4117de3f3d18
bdtgd <2746671038@qq.com>
2025-12-08 00:21:10 +0800

    添加@layer

--- src/component/Cursor/index.js
+++ src/component/Cursor/index.js
@@ -2,8 +2,8 @@ const cursor = (() => {
     const svg = `<svg xmlns="http://www.w3.org/2000/svg" height="39" width="39"><path d="M17.5 2.5h4v13h-4zm0 21h4v13h-4zm-15-6h13v4h-13zm21 0h13v4h-13z" fill="#fff" stroke="#000"/></svg>`;
     const svgUrl = URL.createObjectURL(new Blob([svg], { type: "image/svg+xml" }));
     const styleSheet = new CSSStyleSheet();
-    styleSheet.replaceSync(`*{cursor:url("${svgUrl}") 19 19,default !important}`);
-    document.addEventListener("DOMContentLoaded", () => (document.adoptedStyleSheets = [...document.adoptedStyleSheets, styleSheet]));
+    styleSheet.replaceSync(`@layer noita-cursor{:root{cursor:url("${svgUrl}") 19 19,default}}`);
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, styleSheet];
     return Object.freeze({
         get disable() {
             return styleSheet.disabled;
