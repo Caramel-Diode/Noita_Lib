55e3764d97499e2631c4f1867bf1e136b5b11781
bdtgd <2746671038@qq.com>
2025-12-07 23:35:47 +0800

    优化样式表构建逻辑

--- src/public/cssTools.js
+++ src/public/cssTools.js
@@ -1,8 +1,11 @@
 /**
- * @typedef {{[key : keyof HTMLElementTagNameMap]: {[K in keyof CSSStyleDeclaration as CSSStyleDeclaration[K] extends String ? K :never]: String}} & {
- * ":host":{[K in keyof CSSStyleDeclaration as CSSStyleDeclaration[K] extends String ? K :never]: String};
- * "*":{[K in keyof CSSStyleDeclaration as CSSStyleDeclaration[K] extends String ? K :never]: String};
- * }} CSSStyleMap
+ * @typedef {{[K in keyof CSSStyleDeclaration]: CSSStyleDeclaration[K] extends string ? CSSStyleDeclaration[K] : never} & {[K:string]: StyleAttrs}} StyleAttrs 样式属性
+ */
+
+/** @typedef {`:${"hover"|"dir()"|"visited"|"checked"|"defined"|"open"|"popover-open"|"modal"|"fullscreen"|"picture-in-picture"|"enabled"|"disabled"|"read-only"|"read-write"|"placeholder-shown"|"autofill"|"default"|"indeterminate"|"blank"|"valid"|"invalid"|"in-range"|"out-of-range"|"required"|"optional"|"user-valid"|"user-invalid"|"lang()"|"any-link"|"link"|"local-link"|"target"|"target-within"|"scope"|"playing"|"paused"|"seeking"|"buffering"|"stalled"|"muted"|"volume-locked"|"current"|"past"|"future"|"root"|"empty"|"nth-child()"|"nth-last-child()"|"first-child"|"last-child"|"only-child"|"nth-of-type()"|"nth-last-of-type()"|"first-of-type"|"last-of-type"|"only-of-type"|"host"|"host()"|"host-context()"|"has-slotted"|"active"|"focus"|"focus-visible"|"focus-within"|"is()"|"not()"|"where()"|"has()"|"state()"|"left"|"right"|"first"}`} PseudoClasses*/
+
+/**
+ * @typedef {{[key in (keyof HTMLElementTagNameMap)|"*"|"[attr=]"|PseudoClasses]: StyleAttrs} } CSSStyleMap
  */
 
 /**
@@ -10,40 +13,44 @@
  */
 const css = (() => {
     /**
-     * @param {CSSStyleDeclaration} declaration
+     * @param {StyleAttrs} declaration
+     * @returns {string}
      */
     const cssText = declaration => {
-        const { style } = document.createElement("html");
-        const nestData = [];
-        const cssTexts_important = [];
+        const sheet = new CSSStyleSheet();
+        /** @type {CSSStyleRule} */
+        const { style } = sheet.cssRules[sheet.insertRule(`*{}`)];
+        const result = [];
         for (const prop in declaration) {
-            const data = declaration[prop];
-            if (typeof data === "object") nestData.push(`${prop}{${cssText(data)}}`);
+            /** @type {string} */
+            let data = declaration[prop];
+            if (typeof data === "object") {
+                result.push(`${prop}{${cssText(data)}}`);
+                continue;
+            }
+            data = String(data).trim();
+            // CSS content 属性设置是无效的 这里要单独记录
+            if (prop === "content") result.push(`content:"${data}";`);
             else if (prop.startsWith("--")) style.setProperty(prop, data);
             //允许使用$简化自定义css属性的"--"前缀
             else if (prop.startsWith("$")) style.setProperty("--" + prop.slice(1), data);
             else if (prop in style) {
                 // 修复带有 `!important` 的属性值无法正常设置的问题
-                if (String(data).includes("!important")) {
-                    /** @type {String} */
-                    const text = cssText({ [prop]: data.replace("!important", "").trim() });
-                    cssTexts_important.push(text.replace(";", " !important;"));
-                } else style[prop] = data;
+                if (data.endsWith("!important")) result.push(cssText({ [prop]: data.slice(0, -10) }).slice(0, -1) + " !important;");
+                else style[prop] = data;
             }
         }
-        return style.cssText + cssTexts_important.join("") + nestData.join("");
+        return style.cssText + result.join("");
     };
 
     /**
-     * @param {String | CSSStyleMap & { [key: string]:{[K in keyof CSSStyleDeclaration as CSSStyleDeclaration[K] extends String ? K :never]: String} }|NodeListOf<HTMLStyleElement>|HTMLStyleElement} datas
-     * @param {CSSStyleSheetInit} init
+     * @param {string | CSSStyleMap} [datas]
+     * @param {CSSStyleSheetInit & {name:string}} [init]
      * @returns {CSSStyleSheet}
      */
-    const fn = (datas, init) => {
-        const styleSheet = new CSSStyleSheet(init);
-        const cache = [];
-        const _type = typeof datas;
-        if (_type === "object") {
+    const fn = (datas = "", init = {}) => {
+        if (typeof datas === "object") {
+            const cache = [];
             for (const key in datas) {
                 const data = datas[key];
                 if (key.startsWith("@keyframes")) {
@@ -62,9 +69,16 @@ const css = (() => {
                     cache.push(`${key}{${_cache.join(";")}}`);
                 } else cache.push(`${key}{${cssText(data)}}`);
             }
-            styleSheet.replaceSync(cache.join(""));
-        } else if (_type === "string") styleSheet.replaceSync(datas);
-        return styleSheet;
+            return fn(cache.join(""), init);
+        }
+        datas = String(datas);
+        init.baseURL ??= URL.createObjectURL(new Blob([datas], { type: "text/css" }));
+        init.name ??= "";
+        const sheet = new CSSStyleSheet(init);
+        Reflect.defineProperty(sheet, "href", { value: init.baseURL, configurable: false });
+        sheet.replaceSync(`@layer ${init.name}{${datas}}`);
+        // return (await import(URL.createObjectURL(new Blob([cssText], { type: "text/css" })), { with: { type: "css" } })).default;
+        return sheet;
     };
     return fn;
 })();
